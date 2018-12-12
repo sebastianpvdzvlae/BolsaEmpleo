a2850c683661628baee1921ad547c92496a66158
JuanSulcaGitHub <juanm.sulca@gmail.com>
2018-12-12 09:44:24 -0500

    Fixed Registrar Artesanos

--- testapp/static/javascript/registrarArtesano.js
+++ testapp/static/javascript/registrarArtesano.js
@@ -16,21 +16,19 @@ $(document).ready(function () {
                 $("#canton").find("option").remove();
                 var i = 0;
                 $.map(response.cantones, function (canton) {
-                    console.log(canton);
                     $("#canton").append('<option value = "' + i + '">' + canton.nombre + '</option>');
+                    i++;
+                });
+                $("#parroquia").find("option").remove();
+                i = 0;
+                $.map(provincia.cantones[parseInt($('#canton').find(":selected").val())].parroquias, function (parroquia) {
+                    $("#parroquia").append('<option value = "' + i + '">' + parroquia + '</option>');
                     i++;
                 });
 
             }).fail(function (data, textStatus, xhr) {
                 console.log([data, textStatus, xhr]);
             });
-        $("#parroquia").find("option").remove();
-        var i = 0;
-        $.map(provincia.cantones[parseInt($('#canton').find(":selected").val())].parroquias, function (parroquia) {
-            console.log(parroquia);
-            $("#parroquia").append('<option value = "' + i + '">' + parroquia + '</option>');
-            i++;
-        });
     }).fail(function (data, textStatus, xhr) {
         console.log([data, textStatus, xhr]);
     });
@@ -41,8 +39,13 @@ $(document).ready(function () {
             $("#canton").find("option").remove();
             var i = 0;
             $.map(response.cantones, function (canton) {
-                console.log(canton);
                 $("#canton").append('<option value = "' + i + '">' + canton.nombre + '</option>');
+                i++;
+            });
+            $("#parroquia").find("option").remove();
+            i = 0;
+            $.map(provincia.cantones[parseInt($('#canton').find(":selected").val())].parroquias, function (parroquia) {
+                $("#parroquia").append('<option value = "' + i + '">' + parroquia + '</option>');
                 i++;
             });
         }).fail(function (data, textStatus, xhr) {
@@ -50,25 +53,25 @@ $(document).ready(function () {
         });
     });
     $("#canton").change(function () {
+        console.log("canton changed!");
         $("#parroquia").find("option").remove();
         var i = 0;
-        $.map(provincia.cantones[parseInt($('#provincia').find(":selected").val())].parroquias, function (parroquia) {
-            console.log(parroquia);
+        $.map(provincia.cantones[parseInt($('#canton').find(":selected").val())].parroquias, function (parroquia) {
             $("#parroquia").append('<option value = "' + i + '">' + parroquia + '</option>');
             i++;
         });
     });
 });
 
-function botonGuardar(){
+function registrarArtesano(){
     var tipoIdentificacion = opciones.options[opciones.selectedIndex];
-    var identificacion  = document.getElementById("identificacion");
-    var nombres = document.getElementById("nombres");
-    var apellidos = document.getElementById("apellidos");
-    var direccion = document.getElementById("direccion");
-    var provincia = provincias.options[provincias.selectedIndex];
-    var canton = cantones.options[cantones.selectedIndex];
-    var parroquia = parroquias.options[parroquias.selectedIndex];
+    var identificacion = document.getElementById("txt_id");
+    var nombres = document.getElementById("txt_nombres");
+    var apellidos = document.getElementById("txt_apellidos");
+    var direccion = document.getElementById("txt_direccion");
+    var provincias = $("#provincia").val();//provincia.options[provincia.selectedIndex];
+    var cantones = $("#canton").val();//canton.options[canton.selectedIndex];
+    var parroquias = $("#parroquia").val();//parroquia.options[parroquia.selectedIndex];
     var telf0 = document.getElementById("inputTelefono");
     var table = document.getElementById('idTable');
     var telefono = new Array();
@@ -82,9 +85,9 @@ function botonGuardar(){
     for(i=table.rows.length+1; i<=6; i++){
         telefono.push("");
     }
-    for(i=0; i<6;i++){
+    /*for(i=0; i<6;i++){
         alert(telefono[i]);
-    }
+    }*/
 
     var user = {
         tipoUser : "Artesano",
@@ -95,15 +98,15 @@ function botonGuardar(){
         nombres: nombres.value,
         direccion: direccion.value,
         ubicacion:{
-            provincia: provincia.value,
-            canton: canton.value,
-            parroquia: parroquia.value,
+            provincia: provincia.nombre,
+            canton: provincia.cantones[cantones].nombre,
+            parroquia: provincia.cantones[cantones].parroquias[parroquias],
         },
         telefonos: telefono,
         password: ""
     };
     console.log(user);
-   $.ajax({
+   /*$.ajax({
         url: serverUrl + "/users/",
         type: "POST",
         contentType: "application/json; charset=utf-8",
@@ -115,5 +118,5 @@ function botonGuardar(){
     }).fail(function (data, textStatus, xhr) {
         window.alert("Hubo un error al guardar los datos");            
         console.log([data, textStatus, xhr]);
-    });
+    });*/
 }
