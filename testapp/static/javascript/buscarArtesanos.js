7c44e5660bdcf89e083e870944104c528df49f21
artdafk <33704879+artdafk@users.noreply.github.com>
2018-12-12 09:10:13 -0500

    Edición de Datos - Admin

--- testapp/static/javascript/buscarArtesanos.js
+++ testapp/static/javascript/buscarArtesanos.js
@@ -72,3 +72,72 @@ function tablaArtesanos(page) {
     });
 
 }
+
+function tablaArtesanosAdmin(page) {
+    var txtBusqueda = $("#txtBusqueda").val()
+    if (txtBusqueda == "") {
+        url = serverUrl + "/artesanos/";
+        var data = { page: page, pageSize: pageSize }
+    }
+    else {
+        url = serverUrl + "/services/";
+        var data = { page: page, pageSize: pageSize, service: txtBusqueda }
+    }
+    $.get({ url: url, cache: false, data })
+        .then(function (response) {
+            $("#tablaArtesanos").find("tbody").remove();
+            var total = response.total;
+            var items = response.items;
+            var pages = Math.ceil(total / pageSize);
+            $("#btnNext").addClass('disabled');
+            if (pages > currentPage + 1) {
+                $("#btnNext").removeClass('disabled');
+            }
+            $("#btnBack").addClass('disabled');
+            if (currentPage > 0) {
+                $("#btnBack").removeClass('disabled');
+            }
+            var tbl = document.getElementById("tablaArtesanos");
+            var tblBody = document.createElement("tbody");
+            var id;
+            for (var i = 0; i < items.length; i++) {
+                var fila = document.createElement("tr");
+                var celda = document.createElement("td");
+                var textoCelda = document.createTextNode(items[i]['nombres']);
+                celda.appendChild(textoCelda);
+                fila.appendChild(celda);
+                celda = document.createElement("td");
+                textoCelda = document.createTextNode(items[i]['apellidos']);
+                celda.appendChild(textoCelda);
+                fila.appendChild(celda);
+                celda = document.createElement("td");
+                textoCelda = document.createTextNode(items[i]['servicios']);
+                celda.appendChild(textoCelda);
+                fila.appendChild(celda);
+                celda = document.createElement("td");
+                textoCelda = document.createTextNode(items[i]['telefonos']);
+                celda.appendChild(textoCelda);
+                fila.appendChild(celda);
+                celda = document.createElement("td");
+                textoCelda = document.createTextNode(items[i]['direccion']);
+                celda = documento.createElement("td"); //inicio botón Editar
+                var boton = document.createElement("input");
+                boton.type = "button";
+                boton.value = "Editar Información";
+                boton.onclick = "javascript:redirect(id)";
+                celda.appendChild(textoCelda);
+                celda.appendChild(boton);
+                fila.appendChild(celda);
+                tblBody.appendChild(fila);
+            }
+            tbl.appendChild(tblBody);
+        }).fail(function (data, textStatus, xhr) {
+            window.alert("Error tabla");
+            console.log([data, textStatus, xhr]);
+        });
+
+}
+
+function redirigir(id) {
+    location.href = serverUrl + "/RegistrarArtesano/" + id;
+}
