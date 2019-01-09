b3f44818b87eaf66ce5f3f30483d51095495a8bc
Erika Silva <erikasilva242@hotmail.com>
2019-01-09 07:47:19 -0500

    Registrar información curso. Registrar instructor.

--- testapp/static/javascript/informacionCurso.js
+++ testapp/static/javascript/informacionCurso.js
@@ -1,17 +1,20 @@
+
 var pageSize = 24;
 var currentPage = 0;
-url = serverUrl + "/courses/";
-var i; 
+url = serverUrl + "/courses/"; 
 
-$(document).ready(function () {
+$(document).ready(function() {
+
     var data = { page: currentPage, pageSize: pageSize }
     $.get({ url: url, cache: false, data })
         .then(function (response) {
             $("#cursos").find("option").remove();
-              $.map(response.courses, function (course) {
-                 $("#cursos").append('<option name="' + course.nombre + '" value = "' + course._id + '">' + course.nombre + '</option>');
+            $.map(response.courses, function (course) {
+                $("#cursos").append('<option name="' + course.nombre + '" value = "' + course._id + '">' + course.nombre + '</option>');
+          
             });
             cargarDatos();
+            //cargarTablaInstructores();
         }).fail(function (data, textStatus, xhr) {
             console.log([data, textStatus, xhr]);
         });
@@ -21,44 +24,116 @@ $(document).ready(function () {
 function cargarDatos() {
     var curso;
     var cursoId = $('#cursos').find(":selected").val();
-    console.log(cursoId);
+    url = serverUrl + "/courses/"; 
+
     $.get({ url: url + cursoId, cache: false, data: {} })
         .then(function (response) {
             curso = response;
-            //cambiar formato a yyyy-mm-dd
-            $('#fechaInicio').val(curso.fechaInicio);
+            $('#fechaInicio').val("");
+            $('#fechaFin').val("");
+            if (curso.fechaInicio != null)
+                $('#fechaInicio').val(curso.fechaInicio);
+            if (curso.fechaFin != null)
+                $('#fechaFin').val(curso.fechaFin);
+            if (curso.numParticipantes != "")
+                $('#numParticipantes').val(curso.numParticipantes);
+            if (curso.lugar != "")
+                $('#lugar').val(curso.lugar);
+            if (curso.horario != "")
+                $('#horario').val(curso.horario);
+            cargarTablaInstructores();
+
         }).fail(function (data, textStatus, xhr) {
             console.log([data, textStatus, xhr]);
         });
 }
 
-function registrarInfoCurso() {   
-    var nombreCurso = $("#cursos").val();
+function cargarTablaInstructores() {
+    url = serverUrl + "/instructors/";
+    var data = { page: currentPage, pageSize: pageSize }
+
+    $.get({ url: url, cache: false, data })
+        .then(function(response) {
+
+            $("#tablaInstructores").find("tbody").remove();
+            var total = response.count;
+            var items = response.courses;
+            var pages = Math.ceil(total / pageSize);
+            /*$("#btnNext").addClass('disabled');
+            if (pages > currentPage + 1) {
+                $("#btnNext").removeClass('disabled');
+            }
+            $("#btnBack").addClass('disabled');
+            if (currentPage > 0) {
+                $("#btnBack").removeClass('disabled');
+            }*/
+            var tbl = document.getElementById("tablaInstructores");
+            var tblBody = document.createElement("tbody");
+            
+            
+            for (var i = 0; i < items.length; i++) {
+                var fila = document.createElement("tr");
+                var celda = document.createElement("td");
+                var textoCelda = document.createTextNode(items[i]['identificacion']);
+                celda.appendChild(textoCelda);
+                fila.appendChild(celda);
+                celda = document.createElement("td");
+                textoCelda = document.createTextNode(items[i]['nombres']);
+                celda.appendChild(textoCelda);
+                fila.appendChild(celda);
+                celda = document.createElement("td");
+                textoCelda = document.createTextNode(items[i]['apellidos']);
+                celda.appendChild(textoCelda);
+                fila.appendChild(celda);
+                celda = document.createElement("td");
+                textoCelda = document.createTextNode(items[i]['telefono']);
+                celda.appendChild(textoCelda);
+                fila.appendChild(celda);
+                tblBody.appendChild(fila);
+            }
+
+            tbl.appendChild(tblBody);
+        }).fail(function(data, textStatus, xhr) {
+            window.alert("Error tabla");
+            console.log([data, textStatus, xhr]);
+        });
+
+}
+
+function formato(texto) { //cambiar formato de dd/mm/yyyy a yyyy-mm-dd
+    return texto.replace(/^(\d{2})\/(\d{2})\/(\d{4})$/g, '$3-$2-$1');
+}
+
+function registrarInfoCurso() {
+    var opcCursos = document.getElementById("cursos");
+    var curso = opcCursos.options[opcCursos.selectedIndex];
+    
     var fecha_inicio = document.getElementById("fechaInicio");
 	var fecha_fin = document.getElementById("fechaFin");
 	var numParticipantes = document.getElementById("numParticipantes");
 	var lugar = document.getElementById("lugar");
 	var horario = document.getElementById("horario");
 	
-	var info_curso = {
-		curso: nombreCurso.value,
+    var info_curso = {
+        curso: curso.getAttribute("name"),
 		fecha_inicio: fecha_inicio.value,
 		fecha_fin: fecha_fin.value,
 	    numParticipantes: numParticipantes.value,
 		lugar: lugar.value,
 		horario: horario.value
 	}
-	url='http://127.0.0.1:5000/sessions/';
+	
 	console.log(info_curso);
     $.ajax({
-		url: url,
-		type: "POST",
+        url: url + curso.value,
+		type: "PUT",
 		contentType: "application/json; charset=utf-8",
 		data: JSON.stringify(info_curso),
 		dataType: "json"
 	}).then(function (response) {
 		console.log(response._id);
-		window.alert("Información de Curso registrado con éxito");
+        window.alert("Información de Curso registrado con éxito");
+        window.location.reload(true);
 	}).fail(function (data, textStatus, xhr) {
 		window.alert("Error");
 		console.log([data, textStatus, xhr]);
